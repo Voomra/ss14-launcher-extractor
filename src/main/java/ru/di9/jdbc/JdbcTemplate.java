9e3baaae7359431c6e48df0249f62f1604cc7019
Voomra <dimon550@gmail.com>
2025-03-03 21:04:16 +0300

    remove: излишний функционал

--- src/main/java/ru/di9/jdbc/JdbcTemplate.java
+++ src/main/java/ru/di9/jdbc/JdbcTemplate.java
@@ -3,34 +3,15 @@ package ru.di9.jdbc;
 import org.intellij.lang.annotations.Language;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Consumer;
 
 public interface JdbcTemplate {
 
-    void execute(@Language("GenericSQL") String sql) throws DataAccessException;
-
-    void execute(@Language("GenericSQL") String sql, PreparedStatementProcessor psp) throws DataAccessException;
-
     <T> T query(@Language("GenericSQL") String sql, ResultSetExtractor<T> rse) throws DataAccessException;
 
-    <T> T query(@Language("GenericSQL") String sql, PreparedStatementProcessor psp, ResultSetExtractor<T> rse)
+    <T> void query(@Language("GenericSQL") String sql, PreparedStatementProcessor psp, ResultSetExtractor<T> rse)
             throws DataAccessException;
-
-    <T> Optional<T> queryOne(@Language("GenericSQL") String sql, ResultSetExtractor<T> rse)
-            throws DataAccessException;
-
-    <T> Optional<T> queryOne(@Language("GenericSQL") String sql, PreparedStatementProcessor psp,
-                             ResultSetExtractor<T> rse) throws DataAccessException;
 
     <T> List<T> queryList(@Language("GenericSQL") String sql,
                           final RowMapper<T> rowMapper) throws DataAccessException;
 
-    <T> List<T> queryList(@Language("GenericSQL") String sql, PreparedStatementProcessor psp,
-                          final RowMapper<T> rowMapper) throws DataAccessException;
-
-    <T> T insert(@Language("GenericSQL") String sql, PreparedStatementProcessor psp,
-                 ResultSetExtractor<T> processGeneratedKey) throws DataAccessException;
-
-    void transaction(Consumer<JdbcTemplate> consumer);
 }
--- src/main/java/ru/di9/jdbc/JdbcTemplateImpl.java
+++ src/main/java/ru/di9/jdbc/JdbcTemplateImpl.java
@@ -7,35 +7,13 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Consumer;
 
+@SuppressWarnings("SqlSourceToSinkFlow")
 public class JdbcTemplateImpl implements JdbcTemplate {
     private final DataSource dataSource;
 
     public JdbcTemplateImpl(DataSource dataSource) {
         this.dataSource = dataSource;
-    }
-
-    @Override
-    public void execute(@Language("GenericSQL") String sql) throws DataAccessException {
-        try (Connection connection = dataSource.getConnection();
-             Statement statement = connection.createStatement()) {
-            statement.execute(sql);
-        } catch (SQLException e) {
-            throw throwDataAccessException(sql, e);
-        }
-    }
-
-    @Override
-    public void execute(@Language("GenericSQL") String sql, PreparedStatementProcessor psp) throws DataAccessException {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            psp.process(preparedStatement);
-            preparedStatement.execute();
-        } catch (SQLException e) {
-            throw throwDataAccessException(sql, e);
-        }
     }
 
     @Override
@@ -50,99 +28,23 @@ public class JdbcTemplateImpl implements JdbcTemplate {
     }
 
     @Override
-    public <T> T query(@Language("GenericSQL") String sql, PreparedStatementProcessor psp,
-                       ResultSetExtractor<T> rse) throws DataAccessException {
+    public <T> void query(@Language("GenericSQL") String sql, PreparedStatementProcessor psp,
+                          ResultSetExtractor<T> rse) throws DataAccessException {
         try (Connection connection = dataSource.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             psp.process(preparedStatement);
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                return rse.extractData(resultSet);
+                rse.extractData(resultSet);
             }
         } catch (SQLException e) {
             throw throwDataAccessException(sql, e);
         }
-    }
-
-    @Override
-    public <T> Optional<T> queryOne(@Language("GenericSQL") String sql,
-                                    ResultSetExtractor<T> rse) throws DataAccessException {
-        return query(sql, rs -> {
-            if (rs.next()) {
-                return Optional.ofNullable(rse.extractData(rs));
-            } else {
-                return Optional.empty();
-            }
-        });
-    }
-
-    @Override
-    public <T> Optional<T> queryOne(@Language("GenericSQL") String sql, PreparedStatementProcessor psp,
-                                    ResultSetExtractor<T> rse) throws DataAccessException {
-        return query(sql, psp, rs -> {
-            if (rs.next()) {
-                return Optional.ofNullable(rse.extractData(rs));
-            } else {
-                return Optional.empty();
-            }
-        });
     }
 
     @Override
     public <T> List<T> queryList(@Language("GenericSQL") String sql,
                                  final RowMapper<T> rowMapper) throws DataAccessException {
         return query(sql, createResultSetExtractorList(rowMapper));
-    }
-
-    @Override
-    public <T> List<T> queryList(@Language("GenericSQL") String sql, PreparedStatementProcessor psp,
-                                 final RowMapper<T> rowMapper) throws DataAccessException {
-        return query(sql, psp, createResultSetExtractorList(rowMapper));
-    }
-
-    @Override
-    public <T> T insert(@Language("GenericSQL") String sql, PreparedStatementProcessor psp,
-                        ResultSetExtractor<T> processGeneratedKey) throws DataAccessException {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-            psp.process(preparedStatement);
-            preparedStatement.execute();
-            try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
-                generatedKeys.next();
-                return processGeneratedKey.extractData(generatedKeys);
-            }
-        } catch (SQLException e) {
-            throw throwDataAccessException(sql, e);
-        }
-    }
-
-    @Override
-    public void transaction(Consumer<JdbcTemplate> consumer) {
-        Connection connection = null;
-        try {
-            connection = dataSource.getConnection();
-            connection.createStatement().execute("BEGIN TRANSACTION");
-            consumer.accept(new JdbcTemplateTransactional(connection));
-            connection.createStatement().execute("COMMIT");
-        } catch (SQLException e) {
-            if (connection != null) {
-                try {
-                    connection.createStatement().execute("ROLLBACK");
-                    throw new DataAccessException("Error transaction", e);
-                } catch (SQLException e1) {
-                    DataAccessException exception = new DataAccessException("Error rollback", e1);
-                    exception.addSuppressed(e1);
-                    throw exception;
-                }
-            }
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    throw new DataAccessException("Error close connection", e);
-                }
-            }
-        }
     }
 
     private <T> ResultSetExtractor<List<T>> createResultSetExtractorList(final RowMapper<T> rowMapper) {
